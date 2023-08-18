dad9456b42544f6d2aa436346b418188824a6098
Andrei Khodko <andrei.khodko@gmail.com>
2023-08-18 23:31:55 +0300

    playlist as main queue manager

--- internal/halva-discord-music/music/player/playlist/service.go
+++ internal/halva-discord-music/music/player/playlist/service.go
@@ -1,13 +1,24 @@
 package playlist
 
 import (
+	"math/rand"
+	"sync"
+
 	psong "github.com/HalvaPovidlo/halva-services/internal/pkg/song"
 )
 
+type radioService interface {
+	Radio(minPlaybacks int64) (*psong.Item, error)
+}
+
 type service struct {
 	queue   []psong.Item
+	radio   bool
 	loop    bool
 	shuffle bool
+
+	radioService radioService
+	*sync.Mutex
 }
 
 func New() *service {
@@ -17,49 +28,103 @@ func New() *service {
 }
 
 func (s *service) Add(item *psong.Item) {
+	s.Lock()
 	s.queue = append(s.queue, *item)
+	s.Unlock()
 }
 
-func (s *service) Peek() *psong.Item {
+func (s *service) Head() *psong.Item {
+	s.Lock()
+	defer s.Unlock()
+
+	if len(s.queue) == 0 {
+		if s.radio {
+			if r, err := s.radioService.Radio(3); err == nil {
+				s.queue = append(s.queue, *r)
+				return &s.queue[0]
+			}
+		}
+		return nil
+	}
+
+	if s.loop {
+		return &s.queue[0]
+	}
+
+	if s.shuffle {
+		r := rand.Intn(len(s.queue))
+		q := make([]psong.Item, 0, len(s.queue))
+		q = append(q, s.queue[r])
+		q = append(q, s.queue[:r]...)
+		q = append(q, s.queue[r+1:]...)
+		s.queue = q
+	}
+
+	return &s.queue[0]
+}
+
+func (s *service) Remove(force bool) {
+	s.Lock()
+	if (s.loop && !force) || len(s.queue) == 0 {
+		s.Unlock()
+		return
+	}
+	s.queue = s.queue[1:]
+	s.Unlock()
+}
+
+func (s *service) Current() *psong.Item {
+	s.Lock()
+	defer s.Unlock()
+
 	if len(s.queue) == 0 {
 		return nil
 	}
-	song := s.queue[0]
-	return &song
+
+	return &s.queue[0]
 }
 
 func (s *service) Queue() []psong.Item {
+	s.Lock()
 	queue := make([]psong.Item, len(s.queue))
 	copy(queue, s.queue)
+	s.Unlock()
+
 	return queue
 }
 
-func (s *service) Remove() {
-	if s.loop || len(s.queue) == 0 {
-		return
-	}
-	s.queue = s.queue[1:]
+func (s *service) Loop(state bool) {
+	s.Lock()
+	s.loop = state
+	s.Unlock()
 }
 
-func (s *service) RemoveForce() {
-	if len(s.queue) == 0 {
-		return
-	}
-	s.queue = s.queue[1:]
+func (s *service) LoopToggle() {
+	s.Lock()
+	s.loop = !s.loop
+	s.Unlock()
 }
 
-func (s *service) Loop() {
-	s.loop = true
+func (s *service) Radio(state bool) {
+	s.Lock()
+	s.radio = state
+	s.Unlock()
 }
 
-func (s *service) LoopDisable() {
-	s.loop = false
+func (s *service) RadioToggle() {
+	s.Lock()
+	s.radio = !s.radio
+	s.Unlock()
 }
 
-func (s *service) Shuffle() {
-	s.shuffle = true
+func (s *service) Shuffle(state bool) {
+	s.Lock()
+	s.shuffle = state
+	s.Unlock()
 }
 
-func (s *service) ShuffleDisable() {
-	s.shuffle = false
+func (s *service) ShuffleToggle() {
+	s.Lock()
+	s.shuffle = !s.shuffle
+	s.Unlock()
 }
